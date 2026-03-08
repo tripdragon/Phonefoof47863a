35c2b2e4d195928586b1fc251547f6720bb12e5e
tripdragon <nickitdesign@gmail.com>
2026-03-07 22:02:38 -0500

    Update rubixC-scene.js

--- src/three-demo/rubixC-scene.js
+++ src/three-demo/rubixC-scene.js
@@ -1,6 +1,6 @@
 import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
-import { Primitives‎ } from "superneatlib";
+import { Primitives } from "superneatlib";
 
 export function renderRubixCRoute(container) {
 // CheapPool

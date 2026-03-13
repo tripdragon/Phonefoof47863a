045bbd8e67051d1296111430aec1b09bb45ce017
tripdragon <nickitdesign@gmail.com>
2026-03-13 11:15:12 -0400

    Create fingersAPI.js

---
+++ src/three-demo/rubixC/fingersAPI.js
@@ -0,0 +1,16 @@
+// this will reimplement a lot of superneatlibs touch events
+// which is pointless
+// hrmmmmtttfuuoof
+// ai is gonna have a heavy hand in this one
+// duto the redundancy 
+
+
+export class FingersAPI {
+  constructor(){
+
+  }
+
+  beginPointerDown(ev){
+    
+  }
+}

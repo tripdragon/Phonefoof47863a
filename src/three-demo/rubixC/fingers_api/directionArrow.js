c14a4c0e5a3790c7a728b81a7a26586004e45529
tripdragon <nickitdesign@gmail.com>
2026-07-07 21:14:04 -0400

    absolute vector dir in!! refactor

--- src/three-demo/rubixC/fingers_api/directionArrow.js
+++ src/three-demo/rubixC/fingers_api/directionArrow.js
@@ -10,11 +10,33 @@ export class DirectionArrow{
 
 	ff;
 
-	arrowDirHelper;
-	arrowDirV = new Vector3();
-	arrowDirOriginV = new Vector3();
+	arrows = {
+		average : {
+			visual: null,
+			dirV : new Vector3(),
+			originV : new Vector3(),
+			movingAverageV : new Vector3()
+		},
+		absolute : {
+			visual: null,
+			dirV : new Vector3(),
+			originV : new Vector3(),
+			movingAverageV : new Vector3(),
+			units : [new Vector3(), new Vector3(), new Vector3(), new Vector3()],
+			nearest: [],
+			workV: new Vector3()
+		}
 
-	movingAveragePointV = new Vector3();
+	}
+
+	workV = new Vector3();
+	startV = new Vector3();
+
+	// arrowDirHelper;
+	// arrowDirV = new Vector3();
+	// arrowDirOriginV = new Vector3();
+
+	// movingAveragePointV = new Vector3();
 	// pointDown3D = new Vector3();
 
 	currentDragDistance = 0;
@@ -34,34 +56,61 @@ export class DirectionArrow{
 
 
 	build(){
-		this.arrowDirHelper = new ThickArrowHelper(this.arrowDirV, this.arrowDirOriginV, 5.1, 0xffffff, 0.18, 0.1);
+		const aa = this.arrows.average;
+		// this.arrowDirHelper = new ThickArrowHelper(this.arrowDirV, this.arrowDirOriginV, 5.1, 0xffffff, 0.18, 0.1);
+		aa.visual = new ThickArrowHelper(aa.dirV, aa.originV, 4.1, 0xffffff, 0.18, 0.1);
 		//this.arrowDirHelper.visible = false;
-		this.ff.visualsObject3D.add(this.arrowDirHelper);
+		this.ff.visualsObject3D.add(aa.visual);
+
+		const a2 = this.arrows.absolute;
+		// this.arrowDirHelper = new ThickArrowHelper(this.arrowDirV, this.arrowDirOriginV, 5.1, 0xffffff, 0.18, 0.1);
+		a2.visual = new ThickArrowHelper(a2.dirV, a2.originV, 1.1, 0xffc702, 0.18, 0.1);
+		//this.arrowDirHelper.visible = false;
+		this.ff.visualsObject3D.add(a2.visual);
+
+		
 	}
 
 
 	reset(){
-		this.movingAveragePointV.set(0,0,0);
-		this.arrowDirOriginV.set(0,0,0);
-		this.arrowDirV.set(0,0,0);
+		const aa = this.arrows.average;
+		aa.movingAverageV.set(0,0,0);
+		aa.originV.set(0,0,0);
+		aa.dirV.set(0,0,0);
 	}
 
 	refresh(){
+
+		const aa = this.arrows.average;
+		const a2 = this.arrows.absolute;
+
 		// this.arrowDirOriginV.copy(this.pointsPlane[0].point);
 		const planePoints = this.ff.getPlanePoints();
-		const cubePoints = this.ff.getCubePoints();
+		// const cubePoints = this.ff.getCubePoints();
 		
 		if(planePoints.length > this.waitLimToUpdateAveDir){
 			
 			// this.arrowDirOriginV.copy(cubePoints[0].point);
-			this.arrowDirOriginV.copy(this.ff.getPointDown());
+			aa.originV.copy(this.ff.getPointDown());
 
-			this.arrowDirV.set(0, 0, 1);
-			this.arrowDirHelper.position.copy(this.arrowDirOriginV);
-			this.arrowDirHelper.setDirection(this.arrowDirV);
+			aa.dirV.set(0, 0, 1);
+			aa.visual.position.copy(aa.originV);
+			aa.visual.setDirection(aa.dirV);
 			if (this.showDirectionArrow) {
-			  this.arrowDirHelper.visible = true;
+			  aa.visual.visible = true;
 			}
+
+
+
+			a2.originV.copy(this.ff.getPointDown());
+
+			a2.dirV.set(0, 0, 1);
+			a2.visual.position.copy(a2.originV);
+			a2.visual.setDirection(a2.dirV);
+			if (this.showDirectionArrow) {
+			  a2.visual.visible = true;
+			}
+
 			// this.updateClampedDirectionHelper();
 			// this.updateCrossProductHelper();
 
@@ -107,6 +156,8 @@ export class DirectionArrow{
 		// of the points to get the direction on the plane
 		// then over a threshold, activate the next states
 
+		const aa = this.arrows.average;
+		const a2 = this.arrows.absolute;
 
 		const planePoints = this.ff.getPlanePoints();
 
@@ -116,7 +167,7 @@ export class DirectionArrow{
 		}
 		// this.getAveragePointFromHits(this.pointsPlane, this.movingAveragePointV);
 		// this.getAveragePointFromHits(this.ff.touchesController.session.points.plane, this.movingAveragePointV);
-		this.getAveragePointFromHits(planePoints, this.movingAveragePointV);
+		this.getAveragePointFromHits(planePoints, aa.movingAverageV);
 
 		// const first = planePoints[0].point;
 		const first = this.ff.getPointDown();
@@ -130,16 +181,38 @@ export class DirectionArrow{
 						//   this.popThresholdBubble(this.currentDragDistance);
 						// }
 
-		this.arrowDirV.copy(this.movingAveragePointV).sub(this.arrowDirOriginV);
-		const dirLen = this.arrowDirV.length();
+		// aa.dirV.copy(aa.movingAverageV).sub(aa.originV);
+		aa.dirV.copy(aa.movingAverageV);
+		const dirLen = aa.dirV.length();
 		if (dirLen > this.EPSILONish) {
-		  this.arrowDirV.multiplyScalar(1 / dirLen);
-		  this.arrowDirHelper.position.copy(this.arrowDirOriginV);
-		  this.arrowDirHelper.setDirection(this.arrowDirV);
-		  this.arrowDirHelper.visible = true;
+		  aa.dirV.multiplyScalar(1 / dirLen);
+		  aa.visual.position.copy(aa.originV);
+		  aa.visual.setDirection(aa.dirV);
+		  aa.visual.visible = true;
 						  // this.updateClampedDirectionFromSelectedScreenAxises();
 						  // this.updateCrossProductHelper();
 		}
+
+
+
+
+// a2.dirV.copy(a2.movingAverageV).sub(a2.originV);
+		if (dirLen > this.EPSILONish) {
+		  // a2.dirV.multiplyScalar(1 / dirLen);
+		  a2.visual.position.copy(aa.originV);
+		  // a2.visual.setDirection(aa.dirV);
+		  
+		  const vv = this.getAbsoluteDir(aa.dirV);
+
+		  a2.visual.setDirection(vv);
+		  a2.visual.visible = true;
+						  // this.updateClampedDirectionFromSelectedScreenAxises();
+						  // this.updateCrossProductHelper();
+		}
+
+
+
+
 
 					// // // ok from here we need to atan2 in local space
 					// // // then test north east etc...
@@ -168,18 +241,77 @@ export class DirectionArrow{
 	/*
 	hits: [THREE.Point]
 	*/
+
+	// this is wrong, its not localizing the points
+	// they start in world and thus the first is 0 to world
 	getAveragePointFromHits(hits, output){
+		// window.hh = hits;
 		output.set(0, 0, 0);
 		if(hits.length === 0) return output;
-		hits.forEach(x => {
-		  output.add(x.point);
-		});
+		// hits.forEach(x => {
+		//   output.add(x.point);
+		// });
+		this.startV.copy(hits[0].point)
+		for (var i = 1; i < hits.length; i++) {
+			this.workV.copy(hits[i].point).sub(this.startV);
+			output.add(this.workV);
+		}
 		output.divideScalar(hits.length);
 		return output;
 	}
 
 	nearlyEqual(a, b) {
 	  return Math.abs(a - b) < EPSILON;
+	}
+
+
+
+	getAbsoluteDir(dirIn){
+
+		const a2 = this.arrows.absolute;
+		
+		a2.nearest = Object.entries(dirIn).reduce((a, b) =>
+		  Math.abs(b[1]) < Math.abs(a[1]) ? b : a
+		);
+
+		const [key, value] = a2.nearest;
+		// const units = [new Vector3(), new Vector3(), new Vector3(), new Vector3()];
+
+		const units = a2.units;
+		// its NOT the prop, to the values are absolute n,e,s,w
+		if (key === "x") {
+			units[0].set(0,1,0);
+			units[1].set(0,-1,0);
+			units[2].set(0,0,1);
+			units[3].set(0,0,-1);
+		}
+		else if (key === "y") {
+			units[0].set(1,0,0);
+			units[1].set(-1,0,0);
+			units[2].set(0,0,1);
+			units[3].set(0,0,-1);
+		}
+		else if (key === "z") {
+			units[0].set(1,0,0);
+			units[1].set(-1,0,0);
+			units[2].set(0,1,0);
+			units[3].set(0,-1,0);
+		}
+
+		a2.workV.copy(dirIn).normalize();
+
+		let val = a2.workV.dot(units[0]);
+		let pick = 0;
+		for (var i = 1; i < units.length; i++) {
+			const yy = a2.workV.dot(units[i]);
+			if(val < yy){
+				val = yy;
+				pick = i;
+			}
+		}
+
+		return units[pick];
+
 	}
 
 
